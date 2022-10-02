be33313debbc1a5dfa521bf4a0bff26680c5a430
Franco <francollopard@gmail.com>
2022-10-02 05:25:52 -0300

    cambioss

--- javasscipt.js/prueba2.js
+++ javasscipt.js/prueba2.js
@@ -2,4 +2,4 @@ const multiplicators = (a,b, printText )=>{
     console.log(printText, a * b)
 }
 
-multiplicators(4,'5','Multiplicando numeros: ')
+multiplicators(4,'5','Multiplicando numeros: fefefe')
