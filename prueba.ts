0efdced21f148d47cf2b97f82ddf86f15f95ee91
Franco <francollopard@gmail.com>
2022-10-02 04:08:19 -0300

    first push

---
+++ prueba.ts
@@ -0,0 +1,5 @@
+ const multiplicator = (a: number,b: number, printText: string) =>{
+    console.log(printText, a * b)
+}
+
+multiplicator(4,5,'Multiplied numbers: ')
---
+++ prueba2.js
@@ -0,0 +1,5 @@
+const multiplicators = (a,b, printText )=>{
+    console.log(printText, a * b)
+}
+
+multiplicators(4,'5','Multiplied numbers: ')
